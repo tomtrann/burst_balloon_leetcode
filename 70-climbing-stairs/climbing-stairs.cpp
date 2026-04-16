75715bf81968a4a98dcdb3b44a437a54c72dd108
tomtrann <trancamtoan00@gmail.com>
2026-04-16 03:22:19 -0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.11%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) return n;
+        vector<int> array(n+1);
+        array[1] = 1;
+        array[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            array[i] = array[i-1] + array[i-2];
+        }
+        return array[n];
+    }
+};
