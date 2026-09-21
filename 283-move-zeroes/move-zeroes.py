e1a0e59c2109cb0f401b75224b44bbddb4f957a8
tomtrann__ <trancamtoan00@gmail.com>
2026-09-21 00:20:57 -0500

    Time: 0 ms (100.00%) | Memory: 13.6 MB (20.43%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        l = 0
+        for r in range(len(nums)):
+            if nums[r]: 
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+        return nums
+        
